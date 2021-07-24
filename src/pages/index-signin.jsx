f0d355e70de24406cf0b9a42263f7e073ad4cf04
lafpina <lafpina@outlook.com>
2021-07-24 12:03:19 -0300

    Regenerating Fixed Incomplete SignIn

--- src/pages/index-signin.jsx
+++ src/pages/index-signin.jsx
@@ -1,5 +1,5 @@
-import SignInSide from "../templates/SignInSide";
+// import SignInSide from "../templates/SignInSide";
 
-export default function HomePage() {
-  <SignInSide />
-}
+// export default function HomePage() {
+//   <SignInSide />
+// }
